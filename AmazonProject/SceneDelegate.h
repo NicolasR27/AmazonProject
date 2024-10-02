81a982b8e2d22a243d95550a2669fc0fd03be3f9
Nic Rios <nickrios33@yahoo.com>
2024-10-02 16:20:27 -0700

    Initial Commit

---
+++ AmazonProject/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  AmazonProject
+//
+//  Created by Nicolas Rios on 10/2/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ AmazonProject/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  AmazonProject
+//
+//  Created by Nicolas Rios on 10/2/24.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
